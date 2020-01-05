65ef3d5cd3fc884132527653802646b664a126d9
Christoph Weilenmann <it@puraverdura.ch>
2020-01-05 10:49:38 +0100

    changed single_core image

--- puraverdura/settings.py
+++ puraverdura/settings.py
@@ -212,7 +212,7 @@ IMAGES = {'status_100': '/static/img/status_100_cropped.png',
             'status_0': '/static/img/status_0_cropped.png',
             'single_full': '/static/img/schaufel_pickel_colored.png',
             'single_empty': '/static/img/schaufel_pickel_gray.png',
-            'single_core': '/static/img/schaufel_iconized.png',
+            'single_core': '/static/img/schaufel_pickel_colored.png',
             'core': '/static/img/schaufel_iconized.png'}
 GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
 
