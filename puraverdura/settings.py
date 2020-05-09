2aa0fc9225481961c95330faba69be5be4cb5e04
Christoph Weilenmann <cweilenmann@battrion.com>
2020-05-09 09:25:31 +0200

    changed back Anteilsschein mail without Warteliste

--- puraverdura/settings.py
+++ puraverdura/settings.py
@@ -244,5 +244,5 @@ EMAILS = {
     'welcome': 'puraverdura_emails/willkommen_mail.txt',
     'co_welcome': 'puraverdura_emails/mitabonnent_willkommen.txt',
     'confirm': 'mails/confirm.txt',
-    's_created': 'puraverdura_emails/anteilsschein_mail_warteliste.txt',
+    's_created': 'puraverdura_emails/anteilsschein_mail.txt',
 }
