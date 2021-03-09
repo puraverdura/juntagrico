02914ebaca47c03fa4b08d08d7faeeb93cbc4fe1
Christoph Weilenmann <weilenmc@ethz.ch>
2021-03-09 21:09:34 +0100

    added admin email in setting

--- puraverdura/settings.py
+++ puraverdura/settings.py
@@ -111,6 +111,11 @@ EMAIL_PORT = int(os.environ.get('JUNTAGRICO_EMAIL_PORT', '25' ))
 EMAIL_USE_TLS = os.environ.get('JUNTAGRICO_EMAIL_TLS', 'False')=='True'
 EMAIL_USE_SSL = os.environ.get('JUNTAGRICO_EMAIL_SSL', 'False')=='True'
 
+ADMINS = (
+    ('Admin', os.environ.get('JUNTAGRICO_ADMIN_EMAIL')),
+)
+ 
+
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
 
 WHITELIST_EMAILS = []
