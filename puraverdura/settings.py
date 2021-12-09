3adaadace8d8735598f0349596c3ae7a33109002
Christoph Weilenmann <weilenmc@ethz.ch>
2021-12-09 18:49:51 +0100

    overloaded badge.html to only show image when badge id is 1

--- puraverdura/settings.py
+++ puraverdura/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('JUNTAGRICO_SECRET_KEY')
+#SECRET_KEY = os.environ.get('JUNTAGRICO_SECRET_KEY')
 SECRET_KEY = 'fake-key'
 #DEBUG = os.environ.get("JUNTAGRICO_DEBUG", 'False')=='True'
 DEBUG = True
