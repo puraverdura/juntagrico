4631858bc209efcb2560d3ec28554643dd04d585
Christoph Weilenmann <weilenmc@ethz.ch>
2022-04-12 12:38:33 +0200

    send all messages through one connection

--- puraverdura/puraverdura_mailer.py
+++ puraverdura/puraverdura_mailer.py
@@ -1,5 +1,6 @@
 import copy
 from django.utils import timezone
+from django.core import mail
 
 class Mailer:
     def send(msg):
@@ -12,40 +13,45 @@ class Mailer:
             msg.send()
             return
 
-        msg.get_connection()
+        #msg.get_connection()
+        msgs = []
         plain_msg = copy.copy(msg)
         plain_msg.bcc = []
 
         if msg.to or msg.cc:
             #print('send mail to:')
             #print(plain_msg.recipients())
-            plain_msg.send()
+            #plain_msg.send()
+            msgs.append(plain_msg)
             plain_msg.to = []
             plain_msg.cc = []
 
         bcc_list_of_lists = []
-        timestamps = []
         for i in range(0, num_bccs, email_batch_size):
-            timestamps.append(timezone.now())
             batch_bccs = bccs[i:i + email_batch_size]
             bcc_list_of_lists.append(batch_bccs)
             new_message = copy.copy(plain_msg)
             new_message.bcc = batch_bccs
             #print('send mail to:')
             #print(new_message.recipients())
-            new_message.send()
+            #new_message.send()
+            msgs.append(new_message)
 
 
         admin_msg = copy.copy(plain_msg)
         admin_msg.alternatives = []
-        admin_body = ''
-        for bcc_list, t in zip(bcc_list_of_lists, timestamps):
-            admin_body = admin_body + str(t) + ': ' + str(bcc_list) + ',\n'
+        admin_body = str(timezone.now()) + '\n'
+        for bcc_list in bcc_list_of_lists:
+            admin_body = admin_body + str(bcc_list) + ',\n'
         admin_msg.body = admin_body
         admin_msg.to = ['it@puraverdura.ch']
 
         #print(admin_msg.body)
         #print('send mail to:')
         #print(admin_msg.recipients())
-        admin_msg.send()
+        #admin_msg.send()
+        msgs.append(admin_msg)
+        
+        connection = mail.get_connection()
+        connection.send_messages(msgs)
 
