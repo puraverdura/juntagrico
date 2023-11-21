0bbae5f51db0f65362a02d07cc1f5413cab6139c
Christoph Weilenmann <weilenmc@ethz.ch>
2023-11-21 09:56:39 +0100

    working showing request

--- puraverdura/views_admin.py
+++ puraverdura/views_admin.py
@@ -86,6 +86,7 @@ def send_email_intern(request):
         )
         sent = len(emails)
     request_dict = {k:v[0] for k,v in dict(request.POST).items()}
+    request_dict['emails'] = emails
     return send_email_result(request, numsent=sent, request_dict=request_dict)
     # return redirect('mail-result', numsent=sent, request_dict=request_dict)
 
