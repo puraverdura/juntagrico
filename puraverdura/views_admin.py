3a927a27a9d713f22af34817e79a9a6095c0255a
Christoph Weilenmann <weilenmc@ethz.ch>
2023-11-21 09:43:54 +0100

    test

--- puraverdura/views_admin.py
+++ puraverdura/views_admin.py
@@ -86,7 +86,7 @@ def send_email_intern(request):
         )
         sent = len(emails)
     request_dict = {k:v[0] for k,v in dict(request.POST).items()}
-    request_dict = dict(request.POST)
+    request_dict = {'bla': 1, 'blu':2, 'bli': 3}
     return redirect('mail-result', numsent=sent, request_dict=request_dict)
 
 
