a9d58e1f432a35d1706644a440b0cf0f5af412a2
Leo Roettele <leo.roettele@liip.ch>
2024-12-07 02:18:20 +0100

    Reformat settings.py

--- puraverdura/settings.py
+++ puraverdura/settings.py
@@ -7,7 +7,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('JUNTAGRICO_SECRET_KEY')
 # SECRET_KEY = 'fake-key'
@@ -42,19 +41,21 @@ ROOT_URLCONF = 'puraverdura.urls'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('JUNTAGRICO_DATABASE_ENGINE','django.db.backends.sqlite3'), 
-        'NAME': os.environ.get('JUNTAGRICO_DATABASE_NAME','puraverdura.db'), 
-        'USER': os.environ.get('JUNTAGRICO_DATABASE_USER'), #''junatagrico', # The following settings are not used with sqlite3:
-        'PASSWORD': os.environ.get('JUNTAGRICO_DATABASE_PASSWORD'), #''junatagrico',
-        'HOST': os.environ.get('JUNTAGRICO_DATABASE_HOST'), #'localhost', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': os.environ.get('JUNTAGRICO_DATABASE_PORT', False), #''', # Set to empty string for default.
+        'ENGINE': os.environ.get('JUNTAGRICO_DATABASE_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('JUNTAGRICO_DATABASE_NAME', 'puraverdura.db'),
+        'USER': os.environ.get('JUNTAGRICO_DATABASE_USER'),
+        # ''junatagrico', # The following settings are not used with sqlite3:
+        'PASSWORD': os.environ.get('JUNTAGRICO_DATABASE_PASSWORD'),  # ''junatagrico',
+        'HOST': os.environ.get('JUNTAGRICO_DATABASE_HOST'),
+        # 'localhost', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': os.environ.get('JUNTAGRICO_DATABASE_PORT', False),  # ''', # Set to empty string for default.
     }
 }
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')], 
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'OPTIONS': {
             'context_processors': [
                 'django.contrib.auth.context_processors.auth',
@@ -70,13 +71,12 @@ TEMPLATES = [
                 'django.template.loaders.filesystem.Loader',
                 'django.template.loaders.app_directories.Loader'
             ],
-            'debug' : True
+            'debug': True
         },
     },
 ]
 
 WSGI_APPLICATION = 'puraverdura.wsgi.application'
-
 
 LANGUAGE_CODE = 'de'
 
@@ -96,13 +96,12 @@ DATA_UPLOAD_MAX_NUMBER_FIELDS = 3000
 USE_TZ = True
 TIME_ZONE = 'Europe/Zurich'
 
-DATE_INPUT_FORMATS =['%d.%m.%Y',]
+DATE_INPUT_FORMATS = ['%d.%m.%Y', ]
 
 AUTHENTICATION_BACKENDS = (
     'juntagrico.util.auth.AuthenticateWithEmail',
     'django.contrib.auth.backends.ModelBackend'
 )
-
 
 MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
@@ -117,9 +116,9 @@ MIDDLEWARE = [
 EMAIL_HOST = os.environ.get('JUNTAGRICO_EMAIL_HOST')
 EMAIL_HOST_USER = os.environ.get('JUNTAGRICO_EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('JUNTAGRICO_EMAIL_PASSWORD')
-EMAIL_PORT = int(os.environ.get('JUNTAGRICO_EMAIL_PORT', '25' ))
-EMAIL_USE_TLS = os.environ.get('JUNTAGRICO_EMAIL_TLS', 'False')=='True'
-EMAIL_USE_SSL = os.environ.get('JUNTAGRICO_EMAIL_SSL', 'False')=='True'
+EMAIL_PORT = int(os.environ.get('JUNTAGRICO_EMAIL_PORT', '25'))
+EMAIL_USE_TLS = os.environ.get('JUNTAGRICO_EMAIL_TLS', 'False') == 'True'
+EMAIL_USE_SSL = os.environ.get('JUNTAGRICO_EMAIL_SSL', 'False') == 'True'
 
 FROM_FILTER = {
     # This regex matches @puraverdura.ch email addresses, standalone or in "Name" <email> format.
@@ -130,20 +129,20 @@ FROM_FILTER = {
 ADMINS = (
     ('Admin', os.environ.get('JUNTAGRICO_ADMIN_EMAIL')),
 )
-#DEFAULT_MAILER = 'juntagrico.util.defaultmailer.Mailer'
-#DEFAULT_MAILER = 'puraverdura.puraverdura_mailer.Mailer'
+# DEFAULT_MAILER = 'juntagrico.util.defaultmailer.Mailer'
+# DEFAULT_MAILER = 'puraverdura.puraverdura_mailer.Mailer'
 DEFAULT_MAILER = 'juntagrico.util.mailer.batch.Mailer'
 BATCH_MAILER = {
     'batch_size': 39,
     'wait_time': 65
 }
 
-
 DEFAULT_FROM_EMAIL = 'it@puraverdura.ch'
- 
+
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
 
 WHITELIST_EMAILS = []
+
 
 def whitelist_email_from_env(var_env_name):
     email = os.environ.get(var_env_name)
@@ -155,7 +154,6 @@ if DEBUG is True:
     for key in os.environ.keys():
         if key.startswith("JUNTAGRICO_EMAIL_WHITELISTED"):
             whitelist_email_from_env(key)
-            
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
@@ -183,48 +181,48 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 """
 VOCABULARY = {
     'member': 'Mitglied',
-    'member_pl' : 'Mitglieder',
-    'assignment' : 'Arbeitseinsatz',
-    'assignment_pl' : 'Arbeitseinsätze',
-    'share' : 'Anteilschein',
-    'share_pl' : 'Anteilscheine',
-    'subscription' : 'Ernteanteil',
-    'subscription_pl' : 'Ernteanteile',
-    'co_member' : 'Mitabonnent',
-    'co_member_pl' : 'Mitabonnenten',
-    'price' : 'Jahresbeitrag',
-    'member_type' : 'Mitglied',
-    'member_type_pl' : 'Mitglieder',
-    'depot' : 'Depot',
-    'depot_pl' : 'Depots',
+    'member_pl': 'Mitglieder',
+    'assignment': 'Arbeitseinsatz',
+    'assignment_pl': 'Arbeitseinsätze',
+    'share': 'Anteilschein',
+    'share_pl': 'Anteilscheine',
+    'subscription': 'Ernteanteil',
+    'subscription_pl': 'Ernteanteile',
+    'co_member': 'Mitabonnent',
+    'co_member_pl': 'Mitabonnenten',
+    'price': 'Jahresbeitrag',
+    'member_type': 'Mitglied',
+    'member_type_pl': 'Mitglieder',
+    'depot': 'Depot',
+    'depot_pl': 'Depots',
 }
 ORGANISATION_NAME = "Pura Verdura"
-ORGANISATION_NAME_CONFIG = {"type" : "",
-    "gender" : ""}
+ORGANISATION_NAME_CONFIG = {"type": "",
+                            "gender": ""}
 ORGANISATION_LONG_NAME = "Pura Verdura"
-ORGANISATION_ADDRESS = {"name":"Genossenschaft Pura Verdura", 
-            "street" : "Drusbergstrasse",
-            "number" : "113",
-            "zip" : "8053",
-            "city" : "Zürich",
-            "extra" : ""}
-ORGANISATION_PHONE =''
-ORGANISATION_BANK_CONNECTION = {"PC" : "46-110-7",
-            "IBAN" : "CH38 0839 0036 8201 1000 0",
-            "BIC" : "ABSOCH22XXX",
-            "NAME" : "ABS",
-            "ESR" : ""}
-#INFO_EMAIL = "mitglieder@puraverdura.ch"
+ORGANISATION_ADDRESS = {"name": "Genossenschaft Pura Verdura",
+                        "street": "Drusbergstrasse",
+                        "number": "113",
+                        "zip": "8053",
+                        "city": "Zürich",
+                        "extra": ""}
+ORGANISATION_PHONE = ''
+ORGANISATION_BANK_CONNECTION = {"PC": "46-110-7",
+                                "IBAN": "CH38 0839 0036 8201 1000 0",
+                                "BIC": "ABSOCH22XXX",
+                                "NAME": "ABS",
+                                "ESR": ""}
+# INFO_EMAIL = "mitglieder@puraverdura.ch"
 INFO_EMAIL = '"Pura Verdura" <mitglieder@puraverdura.ch>'
 SERVER_URL = "www.puraverdura.ch"
-#ADMINPORTAL_NAME = "Mitgliederplattform Pura Verdura"
-#ADMINPORTAL_SERVER_URL = "login.puraverdura.ch"
+# ADMINPORTAL_NAME = "Mitgliederplattform Pura Verdura"
+# ADMINPORTAL_SERVER_URL = "login.puraverdura.ch"
 BUSINESS_REGULATIONS = "https://www.puraverdura.ch/wp-content/uploads/20210720_Betriebsreglement.pdf"
 BYLAWS = "https://www.puraverdura.ch/wp-content/uploads/Statuten_Pura-Verdura_Version_2022_nach-GV.pdf"
 MAIL_TEMPLATE = "mails/email.html"
 STYLES = {'static': ['css/individual.css']}
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
-#STYLE_SHEET = "/static/css/individual.css"
+# STYLE_SHEET = "/static/css/individual.css"
 FAVICON = "/static/img/favicono.ico"
 FAQ_DOC = "https://www.puraverdura.ch/faq/"
 EXTRA_SUB_INFO = ""
@@ -236,23 +234,23 @@ CURRENCY = "CHF"
 ASSIGNMENT_UNIT = "ENTITY"
 PROMOTED_JOB_TYPES = []
 PROMOTED_JOBS_AMOUNT = 2
-DEPOT_LIST_GENERATION_DAYS = [1,2,3,4,5,6,7]	
+DEPOT_LIST_GENERATION_DAYS = [1, 2, 3, 4, 5, 6, 7]
 BILLING = False
-BUSINESS_YEAR_START = {"day":1, "month":4}
+BUSINESS_YEAR_START = {"day": 1, "month": 4}
 BUSINESS_YEAR_CANCELATION_MONTH = 12
 MEMBERSHIP_END_MONTH = 12
 USE_JOB_STATUS_IMAGES = True
 IMAGES = {'status_100': '/static/img/status_100_cropped.png',
-            'status_75': '/static/img/status_75_cropped.png',
-            'status_50': '/static/img/status_50_cropped.png',
-            'status_25': '/static/img/status_25_cropped.png',
-            'status_0': '/static/img/status_0_cropped.png',
-            'single_full': '/static/img/schaufel_pickel_colored.png',
-            'single_empty': '/static/img/schaufel_pickel_gray.png',
-            'single_core': '/static/img/schaufel_pickel_colored.png',
-            #'core': '/static/img/schaufel_iconized.png'}
-            #'core': '/static/img/Transparent_Pixel.png'
-            'core': '/static/img/samen_iconized.png'}
+          'status_75': '/static/img/status_75_cropped.png',
+          'status_50': '/static/img/status_50_cropped.png',
+          'status_25': '/static/img/status_25_cropped.png',
+          'status_0': '/static/img/status_0_cropped.png',
+          'single_full': '/static/img/schaufel_pickel_colored.png',
+          'single_empty': '/static/img/schaufel_pickel_gray.png',
+          'single_core': '/static/img/schaufel_pickel_colored.png',
+          # 'core': '/static/img/schaufel_iconized.png'}
+          # 'core': '/static/img/Transparent_Pixel.png'
+          'core': '/static/img/samen_iconized.png'}
 
 GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
 
@@ -279,12 +277,12 @@ GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
 
 EMAILS = {
     'welcome': 'puraverdura_emails/willkommen_mail.txt',
-#    'welcome': 'puraverdura_emails/willkommen_mail_warteliste.txt',
+    # 'welcome': 'puraverdura_emails/willkommen_mail_warteliste.txt',
     'co_welcome': 'puraverdura_emails/mitabonnent_willkommen.txt',
     's_created': 'puraverdura_emails/anteilsschein_mail.txt',
-#    's_created': 'puraverdura_emails/anteilsschein_mail_warteliste.txt',
+    # 's_created': 'puraverdura_emails/anteilsschein_mail_warteliste.txt',
     's_canceled': 'puraverdura_emails/subscription_canceled_mail.txt',
 }
 
 # Admin shell
-ADMIN_SHELL_ONLY_DEBUG_MODE = False
+ADMIN_SHELL_ONLY_DEBUG_MODE = True
